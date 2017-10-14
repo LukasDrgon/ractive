f37d666b3034ef52a82ee0155268c04b0f8a7077
Joseph <fskreuz@gmail.com>
2017-10-14 16:44:26 -0400

    Adding ractive-event-keys

---
+++ packages/ractive-event-keys/test/karma.conf.js
@@ -0,0 +1,12 @@
+const baseConf = require('../../../karma.conf')
+
+module.exports = function (config) {
+  config.set(Object.assign({}, baseConf, {
+    files: [
+      '../../../node_modules/simulant/dist/simulant.umd.js',
+      '../node_modules/ractive/dist/lib.umd.js',
+      '../dist/lib.umd.js',
+      '../tmp/test.umd.js'
+    ]
+  }))
+}
